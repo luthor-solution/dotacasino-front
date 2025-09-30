c2f3162bd40cad889c0e0fcde984e2c4a05305cc
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-29 18:23:28 -0700

    fix rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,11 @@ module.exports = {
   images: {
     domains: ["static.cdneu-stat.com"],
   },
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: '/resources/:path*',
         destination: 'http://cdn.lvslot.net/resources/:path*',
-        permanent: false,
       },
     ]
   },
