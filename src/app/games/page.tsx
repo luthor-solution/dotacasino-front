b6dbb0c167b6f35ca766033d99f571e88c894401
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-11-14 15:41:35 -0700

    use client

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import GamesView from "./Games.client";
 
 const Page = () => {
-  return <GamesView />;
+  return (
+    <Suspense>
+      <GamesView />
+    </Suspense>
+  );
 };
 
 export default Page;
